95d825cf8476aaf060531f2388f41711a53f617e
Parmedio <marco.da.pieve@gmail.com>
2024-02-04 16:35:16 +0100

    refactoring question 4

--- script soluzioni/Q04.js
+++ script soluzioni/Q04.js
@@ -4,10 +4,19 @@ const secretKey = 'bgvyzdsv'
 
 let raccoltaHash = []
 
-for (let i = 1000000; i <= 2000000; i++) {
-  if( md5(secretKey + i).slice(0, 6) === '000000') {
+console.time('timer01');
+
+for (let i = 0; i <= 2000000; i++) {
+  if (raccoltaHash.length > 0)
+    break; 
+  if(md5(secretKey + i).slice(0, 6) === '000000') {
     raccoltaHash.push(i);
   }
 }
 
-console.log(raccoltaHash)
+console.log(raccoltaHash[0])
+
+console.timeEnd('timer01');
+
+// aggiungere "type": "module" in package.json
+// node "script soluzioni/Q04.js"
