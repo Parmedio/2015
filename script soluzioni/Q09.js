8e80281b03fb1db89167d483844e27b49e974b20
Parmedio <marco.da.pieve@gmail.com>
2024-03-02 12:59:50 +0100

    refactoring question 9

--- script soluzioni/Q09.js
+++ script soluzioni/Q09.js
@@ -5,21 +5,171 @@ const [, , fileData] = argv;
 
 // node "script soluzioni/Q09.js" 09
 
-let rows = splitInstructions(fileData);
-let orderedRoutes = createRoutes(rows).sort(function(a, b) {
-    return parseInt(b.distance) - parseInt(a.distance);
-});
+//#region COMMON LOGIC
 
-for (let i = 0; i < orderedRoutes.length; i++) {
-    if (itSOkToRemove(orderedRoutes[i], orderedRoutes)) {
-        orderedRoutes.splice(i, 1); // Rimuovi l'elemento corrente
-        i--; // Decrementa l'indice per compensare la rimozione dell'elemento
-    }
+    console.clear();
+    let rows = splitInstructions(fileData);
+    let routes = createRoutes(rows);
+    const totalNumberOfCities = Object.keys(trackAllCities(routes)).length;
+    const listOfCities = Object.keys(trackAllCities(routes));
+    // console.log(totalNumberOfCities);
+    // console.log(listOfCities);
+
+//#endregion
+
+//#region PART 01
+
+    //#region ATTEMPT 01
+
+        // let orderedRoutes = orderRoutesByDistance(routes, "desc");
+        // let shortestPaths = scrumRoutes(orderedRoutes);
+        // let shortestWay = computePathsDistanceSum(shortestPaths);
+
+        // console.log(shortestPaths);
+        // console.log(trackAllCities(shortestPaths));
+        // console.log(shortestWay);
+
+    //#endregion
+
+    //#region ATTEMPT 02
+
+        // parto dal basso
+        // prendo la prima riga di tragitti e la trascrivo in una lista 'tragitti'
+        // aggiungo entrambe le città ad una lista 'città da cercare'
+        // quale è il primo tragitto con una delle città in 'città da cercare'?
+        // quando trovo salvo tragitto in 'tragitti' e aggiungo altra città in 'città da cercare' (attenzione! in riga che trovo devo collegare solo 1 città a meno che non rimangono altre città da cercare perché altrimenti significa che sto chiudendo a cerchio)
+
+        // 3 liste:
+        // - tutte città
+        // - città già collegate
+        // - città da collegare
+
+    //#endregion
+
+    //#region ATTEMPT 03
+
+        const allPossibleCombination = permute(listOfCities);
+        let shortestPathLength = findShortestPathLength(allPossibleCombination);
+        console.log(shortestPathLength);
+
+    //#endregion
+
+//#endregion
+
+//#region PART 02
+
+    //#region ATTEMPT 03
+
+        let longhestPathLength = findLonghestPathLength(allPossibleCombination);
+        console.log(longhestPathLength);
+
+    //#endregion
+
+//#endregion
+
+//#region LOGIC SECTION
+
+function findLonghestPathLength(listOfCombination) {
+    let longhest = 0;
+
+    listOfCombination.forEach(curr => {
+        let pathLength = calculatePathLength(curr, routes)
+        
+        if (longhest == 0 || longhest < pathLength)
+            longhest = pathLength
+    });
+    
+    return longhest;
 }
 
-console.log(orderedRoutes);
+function findShortestPathLength(listOfCombination) {
+    let shortest = 0;
 
-function checkCityCounts(obj) { // OK
+    listOfCombination.forEach(curr => {
+        let pathLength = calculatePathLength(curr, routes)
+        
+        if (shortest == 0 || shortest > pathLength)
+            shortest = pathLength
+    });
+    
+    return shortest;
+}
+
+function permute(elementList) {
+    const result = [];
+
+    function generate(currentPermutation, remainingArray) {
+        if (remainingArray.length === 0) {
+            result.push(currentPermutation);
+            return;
+        }
+
+        for (let i = 0; i < remainingArray.length; i++) {
+            const newPermutation = currentPermutation.concat(remainingArray[i]);
+            const newArray = [...remainingArray.slice(0, i), ...remainingArray.slice(i + 1)];
+            generate(newPermutation, newArray);
+        }
+    }
+
+    generate([], elementList);
+
+    return result;
+}
+
+function calculatePathLength(path, routes) {
+    let totalDistance = 0;
+
+    for (let i = 0; i < path.length - 1; i++) {
+        const cityDep = path[i];
+        const cityArr = path[i + 1];
+        
+        for (const route of routes) {
+            if ((route.cityDep === cityDep && route.cityArr === cityArr) ||
+                (route.cityDep === cityArr && route.cityArr === cityDep)) {
+                totalDistance += route.distance;
+                break;
+            }
+        }
+    }
+
+    return totalDistance;
+}
+
+function computePathsDistanceSum(pathList){
+    return pathList.reduce((acc, path) => {
+        acc += path.distance;
+        return acc;
+    }, 0);
+}
+
+function scrumRoutes(list){
+    let copiedList = [...list];
+    
+    for (let i = 0; i < copiedList.length; i++) {
+        if (itSOkToRemove(copiedList[i], copiedList)) {
+            copiedList.splice(i, 1);
+            i--;
+        }
+    }
+    
+    return copiedList;
+}
+
+function orderRoutesByDistance(list, way){
+    return list.sort(function(a, b) {
+        if (way == "desc")
+            return b.distance - a.distance;
+        if (way == "asc")
+            return a.distance - b.distance;
+    });
+}
+
+function canBuildPath(routeList) {
+    // logic that verify if can build a path among all cities
+    return true;
+}
+
+function checkCityCounts(obj) {
     let citiesWithOne = 0;
     let citiesWithTwoOrMore = 0;
 
@@ -32,10 +182,15 @@ function checkCityCounts(obj) { // OK
         }
     }
 
-    return citiesWithOne <= 2 && citiesWithTwoOrMore >= Object.keys(obj).length - 2;
+    let test = 
+        citiesWithOne <= 2 && 
+        citiesWithTwoOrMore >= totalNumberOfCities - 2 && 
+        citiesWithOne + citiesWithTwoOrMore == totalNumberOfCities;
+
+    return test;
 }
 
-function areObjectsEqual(obj1, obj2) { // OK
+function areObjectsEqual(obj1, obj2) {
     const keys1 = Object.keys(obj1);
     const keys2 = Object.keys(obj2);
 
@@ -55,12 +210,12 @@ function areObjectsEqual(obj1, obj2) { // OK
 function itSOkToRemove(objToRemove, list){
     let copiedList = [...list];
 
-    copiedList = copiedList.filter(obj => areObjectsEqual(obj, objToRemove));
+    copiedList = copiedList.filter(obj => !areObjectsEqual(obj, objToRemove));
 
-    return checkCityCounts(trackAllCities(copiedList));
+    return checkCityCounts(trackAllCities(copiedList)) && canBuildPath(copiedList);
 }
 
-function trackAllCities(list) { // OK
+function trackAllCities(list) {
     let uniqueCities = new Set();
     
     list.forEach(route => {
@@ -82,13 +237,13 @@ function trackAllCities(list) { // OK
     }, {});
 };
 
-function createRoutes(list) { // OK
+function createRoutes(list) {
     return list.map(x => {
         let rowParts = x.split(' ');
         return {
             cityDep : rowParts[0],
             cityArr : rowParts[2],
-            distance : rowParts[4]
+            distance : parseInt(rowParts[4])
         }     
     })
 };
@@ -99,8 +254,4 @@ function splitInstructions(nomeFile) {
     return rows.map(x => x.replace('\r', ''));
 };
 
-
-// ho sbagliato completamente logica, devo partire dal basso, aggiungere e fare il test
-// appena ottengo una situazione con requisiti minimi devo togliere superfluo 
-
-// in realtà potrebbe essere sufficiente scremare comunque dall'alto e scendere fino alla fine
+//#endregion
